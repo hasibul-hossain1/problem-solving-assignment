aa398e3ea581976b12accbe4ef6e3f470feb5d32
shanto <shanto22e@gmail.com>
2025-08-17 22:09:53 +0600

    solve problem no 6

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 // ========= problem  no 1  ==========
 
-// const reverseString=(string)=>{
-//     return string.split('').reverse().join('')
-// }
-
+const reverseString=(string)=>{
+    return string.split('').reverse().join('')
+}
 
 
 // =========  problem no 2  ==========
 
-// const countVowels=(string) => {
-//     return string.split('').reduce((acc,curr)=>{
-//         let vowel='aeiou'
-//         !!vowel.includes(curr.toLowerCase()) && acc++
-//         return acc
-//     },0)
-// }
+const countVowels=(string) => {
+    return string.split('').reduce((acc,curr)=>{
+        let vowel='aeiou'
+        !!vowel.includes(curr.toLowerCase()) && acc++
+        return acc
+    },0)
+}
 
 
 // =========  problem no 3  ==========
 
-// const isPalindrome=(string)=>{
-//     return string===string.split('').reverse().join('')
-// }
+const isPalindrome=(string)=>{
+    return string===string.split('').reverse().join('')
+}
 
 // =========  problem no 4  ==========
 
-// const maxNumber=(arr)=>{
-//     return Math.max(...arr)
-// }
+const maxNumber=(arr)=>{
+    return Math.max(...arr)
+}
 
 
 // =========  problem no 5  ==========
@@ -35,6 +34,13 @@
 const removeDuplicate=(arr)=>{
     return [...new Set(arr)]
 }
+
+// =========  problem no 5  ==========
+
+const sumAll=(arr)=>{
+    return arr.reduce((acc,curr)=>acc+curr,0)
+}
+
 
 
 
